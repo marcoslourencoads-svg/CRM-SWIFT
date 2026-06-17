6b0a7b092b0a8488efddfbd1938d0e65d5ca3ace
marcoslourencoads-svg <marcoslourenco.ads@gmail.com>
2026-06-16 22:47:25 -0300

    Update team-chat.module.ts

--- api/src/modules/team-chat/team-chat.module.ts
+++ api/src/modules/team-chat/team-chat.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TeamChatController } from './team-chat.controller';
 import { TeamChatService } from './team-chat.service';
 import { TeamChatPublisher } from './team-chat.publisher';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [TeamChatController],
   providers: [TeamChatService, TeamChatPublisher],
   exports: [TeamChatService],
